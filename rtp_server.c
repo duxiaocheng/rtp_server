b2b433b6c64766e66bc3afb68beda5b450790b61
Chason DU <chason.du@nokia-sbell.com>
2017-08-17 01:36:21 +0000

    Test log method.

--- rtp_server.c
+++ rtp_server.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <syslog.h>
 
 int main()
 {
-  printf("Hello, I am rtp_server!\n");
+  printf("Hello, here is rtp_server!\n");
+  printf("I am printf log to STDOUT.\n");
+
+  syslog(LOG_INFO, "I am syslog.\n");
+
   return 0;
 }
 
